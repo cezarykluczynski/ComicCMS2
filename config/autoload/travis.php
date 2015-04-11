044d15f30b04e521cbc85f8f8c10895468b903d8
Cezary Kluczynski <cezary.kluczynski@gmail.com>
2015-04-11 23:56:28 +0200

    DB config for travis

---
+++ config/autoload/travis.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Configuration for Travis, not used locally or on production.
+ * This file will be moved to local.php before_script phase of travis build.
+ */
+
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'params' => array(
+                    'user'     => 'postgres',
+                )
+            )
+        )
+    ),
+);
