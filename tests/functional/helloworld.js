0294f0b2b86431298f09b22966c27743dfb82728
Cezary Kluczynski <cezary.kluczynski@gmail.com>
2015-05-01 22:28:54 +0200

    Bring ZF2 error into view

--- tests/functional/helloworld.js
+++ tests/functional/helloworld.js
@@ -10,17 +10,14 @@ define([
     registerSuite({
         name: "Main page.",
 
-        /** Check if link to profile configuration is added. */
         "Main page is loading.": function () {
             return this.remote
                 .get( mainPage )
-                .setFindTimeout( 1000 )
-                /** Check if $panel property is null at first. */
-                .execute( function () {
-                    document.getElementsByTagName( "table" )[ 0 ].style.width = "500px";
+                .setExecuteAsyncTimeout( 10000 )
+                .executeAsync( function () {
+                    window.scrollTo( 1000, 0 );
                 });
                 // .setFindTimeout( 3000 )
-                // /** Open panel by clicking entry. */
                 // .findByCssSelector( "body.application" )
                 // .isDisplayed()
                 // .then( function ( visible ) {
@@ -29,15 +26,15 @@ define([
         }
     });
 
+    return;
+
     registerSuite({
         name: "Admin auth.",
 
-        /** Check if link to profile configuration is added. */
         "Panel is loading.": function () {
             return this.remote
                 .get( signIn )
                 .setFindTimeout( 3000 )
-                /** Open panel by clicking entry. */
                 .findByCssSelector( "div.admin-auth" )
                 .isDisplayed()
                 .then( function ( visible ) {
