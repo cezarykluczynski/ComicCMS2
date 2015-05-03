bf9cdc67078f4a9aaa47b98b5d1693eda8dad10d
Cezary Kluczynski <cezary.kluczynski@gmail.com>
2015-05-03 20:59:17 +0200

    Test for admin controllers guards - one can never be too sure

---
+++ module/Admin/test/AdminTest/ACL/AdminGuards.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace AdminTest\ACL;
+
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AdminGuardsTest extends AbstractHttpControllerTestCase
+{
+    public function setUp()
+    {
+        $this->setApplicationConfig(include 'config/application.config.php');
+        parent::setUp();
+    }
+
+    /**
+     * Helper for detecting console routes.
+     * @return boolean
+     */
+    public function isConsoleRouting($controller, $action)
+    {
+        $config = $this->getApplication()->getServiceManager()->get('Config');
+
+        foreach($config['console']['router']['routes'] as $consoleRoute)
+        {
+            if (isset($consoleRoutes['options']['default']))
+            {
+                $currentConfig = $consoleRoutes['options']['default'];
+                if ($controller === $currentConfig['controller'] AND $action === $currentConfig['action'])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks config for any admin-related routes, if they are only accesible by role admin.
+     * You can never be too sure with this.
+     */
+    public function testAdminRelatedControllersAreOnlyAccessibleByAdmin()
+    {
+        $config = $this->getApplication()->getServiceManager()->get('Config');
+
+        if (!isset($config['bjyauthorize']['guards']) || empty($config['bjyauthorize']['guards'])) {
+            $this->assertTrue(false, 'BjyAuthorize guards are missing.');
+
+            return;
+        }
+
+        $guards = $config['bjyauthorize']['guards']['BjyAuthorize\Guard\Controller'];
+
+        foreach($guards as $guard)
+        {
+            /** Non-admin controllers are not a subject of this test. */
+            if (stripos($guard['controller'], 'Admin') === false || stripos($guard['controller'], 'Auth') !== false)
+            {
+                continue;
+            }
+
+            /** Console routing are not subject to ACL. */
+            if ($this->isConsoleRouting($guard['controller'], $guard['action']))
+            {
+                continue;
+            }
+
+            if (count($guard['roles']) !== 1 OR !in_array('admin', $guard['roles']))
+            {
+                $this->assertTrue(false, 'Controller '.$guard['controller'].'::'.$guard['action'].' is not secured.');
+            }
+        }
+    }
+}
