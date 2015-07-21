851ce33ee9958479fc39eb9a4cb17c9d9188616f
Cezary Kluczynski <cezary.kluczynski@gmail.com>
2015-07-21 22:56:47 +0200

    More tweaks to BrowserStack config

--- tests/intern-ci.js
+++ tests/intern-ci.js
@@ -1,14 +1,12 @@
 define({
     suites: false,
-    proxyPort: 9090,
-    proxyUrl: "http://localhost:9090/",
+    proxyPort: 9000,
+    proxyUrl: "http://localhost:9000/",
     capabilities: {
-        "selenium-version": "2.45.0"
+        "selenium-version": "2.45.0",
+        "idle-timeout": 45
     },
     tunnel: "BrowserStackTunnel",
-    tunnelOptions: {
-        port: 4444
-    },
     environments: [
         { browserName: "Firefox" },
         { browserName: "Internet Explorer", platform: "Windows 8.1", version: "11" },
